2e91679e84950fd9afe26fde24249081a65833c5
donataizekor <donataizekor@gmail.com>
2023-11-21 16:09:32 +0000

    changed some variable names

--- script.js
+++ script.js
@@ -1,64 +1,83 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
 
+//initializing app
 const appSettings = {
     //databaseURL is an actual keyword
     databaseURL : "https://grumpy-428d3-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
-const database = getDatabase(app)
 
+//db
+const database = getDatabase(app)
 const refFoodToDB = ref(database, 'food')
 
-const inputEl = document.querySelector('.add-item #input-field')
-const quantityEl = document.querySelector('.add-item #quantity')
-const bBeforeEl = document.querySelector('.add-item #b-before')
-const typeEl = document.querySelector('.add-item #type-list')
+//addItem.html
+const itemInput = document.querySelector('#item-input')
+const quantityInput = document.querySelector('#quantity-input')
+const bBeforeInput = document.querySelector('#b-before-input')
+const locationInput = document.querySelector('#location-input')
 
-const dateEl = document.querySelector('.item-list .date')
-const itemEl = document.querySelector('.item-list .item-info')
-const locationEl = document.querySelector('.item-list .item-location')
+const btnAddItem = document.querySelector('.btn-addItem')
 
 
-const itemList = document.querySelector('.container-list')
-
-const addBtn = document.querySelector('.buttons .add')
-
+//itemsRender.html
+const itemList = document.querySelector('.container-list-items')
+const dateEl = document.querySelector('.item-date')
+const itemEl = document.querySelector('.item-info')
+const locationEl = document.querySelector('.item-location')
 
 
 let itemArray = []
 
 function addItem(){
-   const inputValue = inputEl.value
-   const quantityValue = quantityEl.value
-   const bBeforeValue = bBeforeEl.value
-   const typeValue = typeEl.value
+   const inputValue = itemInput.value
+   const quantityValue = quantityInput.value
+   const bBeforeValue = bBeforeInput.value
+   const locationValue = locationInput.value
    
    if(inputValue === ""){
     alert("please enter item")
    }else{
-    itemArray.push(inputValue, quantityValue, bBeforeValue, typeValue)
+    itemArray.push(inputValue, quantityValue, bBeforeValue, locationValue)
     push(refFoodToDB, itemArray)
- 
     resetInput()
    }
 }
 
 function resetInput(){
-    inputEl.value = ""
-    quantityEl.value = ""
-    bBeforeEl.value = ""
-    typeEl.value = ""
+    itemInput.value = ""
+    quantityInput.value = ""
+    bBeforeInput.value = ""
+    locationInput.value = ""
 
     itemArray = []
 }
 
+//listening to changes from the database
+onValue(refFoodToDB, function(snapshot){
+    if(snapshot.exists()){
+        const foodArray = Object.entries(snapshot.val())
+
+        clearPage()
+        for (let i of foodArray){
+            let currentItem = i
+            let [currentItemID, currentItemArrayofValues] = currentItem
+            appendItem(currentItem)            
+        }
+    }else{
+        itemList.innerHTML = `<p>No items added yet</p>`
+    }
+})
+
+
+//appending items to itemsRender.html
 function appendItem(item){
     let [itemID, itemValue] = item
     
     itemList.innerHTML += `
-            <div class="item-list">
+            <div class="item-row">
                 <div class="date">
                     <h3>Best before</h3>
                     <p>${dateEl.innerHTML = itemValue[2]}</p>
@@ -68,40 +87,36 @@ function appendItem(item){
                     <h3>Item</h3>
                     <p>${itemEl.innerHTML = `${itemValue[1]}, ${itemValue[0]}`}</p>
                 </div>
+                <div class="item-location">
+                    <h3>Location</h3>
+                    <p>${locationEl.innerHTML = itemValue[3]}</p>
+                </div>
                 <div>
                     <button>x</button>
-                    <div>
+                <div>
             </div>
         `
 
-    const delBtn = document.querySelector('.item-list button')
-    delBtn.addEventListener('click', function (){
+    //deleting item from db
+    const delBtn = document.querySelector('.item-row button')
+    delBtn.addEventListener('dblclick', function (){
         const itemExactLocationInDB = ref(database, `food/${itemID}`)
-
-        remove(itemExactLocationInDB)
-
-        
+        remove(itemExactLocationInDB)    
     })
 }
 
-function clearDB(){
+//clearing the page from the previous version so the new one can be appended
+function clearPage(){
     itemList.innerHTML = ""
 }
 
-onValue(refFoodToDB, function(snapshot){
-    if(snapshot.exists()){
-        const foodArray = Object.entries(snapshot.val())
 
-        clearDB()
-        for (let i of foodArray){
-            let currentItem = i
-            let [currentItemID, currentItemArrayofValues] = currentItem
-            appendItem(currentItem);
-        }
-    }else{
-        itemList.innerHTML = `<p>Not items added yet</p>`
-    }
+// const cards = document.querySelectorAll('.card')
+//         for (let i of cards){
+//             i.addEventListener('click', (e) =>{
 
-})
-
-addBtn.addEventListener('click', addItem)
+//                 if(e.target.classList.contains('fridge')){
+//                     console.log('yayy');
+//                 };
+//             })
+//         }
